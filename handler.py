8f43a5e17dc8888566bda8a71ef951dc080ebdea
Avtarsingh <109199789+avt1705@users.noreply.github.com>
2026-09-14 20:12:45 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 import runpod
 
 def handler(event):
-    print("Handler received event:", event)
+    print(">>> Handler received event:", event)
     return {"output": "Hello world"}
 
 runpod.serverless.start({"handler": handler})
